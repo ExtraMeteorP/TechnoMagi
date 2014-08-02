0b609222c1f520484c1a91b7124bcf3280c2dde4
Ollie Read <labs@ollieread.com>
2014-08-02 14:45:04 +0100

    Added extra ability icons and added to init

--- src/main/java/com/ollieread/technomagi/common/init/Abilities.java
+++ src/main/java/com/ollieread/technomagi/common/init/Abilities.java
@@ -5,7 +5,9 @@ import org.apache.logging.log4j.Level;
 import com.ollieread.ennds.ability.IAbilityActive;
 import com.ollieread.ennds.ability.IAbilityPassive;
 import com.ollieread.technomagi.TechnoMagi;
+import com.ollieread.technomagi.ability.active.ActiveAbilityBlink;
 import com.ollieread.technomagi.ability.active.ActiveAbilityFire;
+import com.ollieread.technomagi.ability.active.ActiveAbilityFireball;
 import com.ollieread.technomagi.ability.active.ActiveAbilityFlashstep;
 import com.ollieread.technomagi.ability.active.ActiveAbilityHarden;
 import com.ollieread.technomagi.ability.active.ActiveAbilityInvisibility;
@@ -17,9 +19,11 @@ public class Abilities
 {
 
     public static IAbilityActive abilityActiveFire;
+    public static IAbilityActive abilityActiveFireball;
     public static IAbilityActive abilityActiveHarden;
     public static IAbilityActive abilityActiveInvisibility;
     public static IAbilityActive abilityActiveFlashstep;
+    public static IAbilityActive abilityActiveBlink;
 
     public static IAbilityPassive abilityPassiveScholarResearch;
     public static IAbilityPassive abilityPassiveNanites;
@@ -30,9 +34,11 @@ public class Abilities
         TechnoMagi.logger.log(Level.INFO, "Initiating & registering abilities");
 
         abilityActiveFire = new ActiveAbilityFire("fire");
+        abilityActiveFireball = new ActiveAbilityFireball("fireball");
         abilityActiveHarden = new ActiveAbilityHarden("harden");
         abilityActiveInvisibility = new ActiveAbilityInvisibility("invisibility");
         abilityActiveFlashstep = new ActiveAbilityFlashstep("flashstep");
+        abilityActiveBlink = new ActiveAbilityBlink("blink");
 
         abilityPassiveScholarResearch = new PassiveAbilityScholarResearch("scholarResearch");
         abilityPassiveNanites = new PassiveAbilityNanites("naniteRegen");
