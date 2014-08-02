c4a1faf9eb019077f5c6c4954195cc224b206ac2
Ollie Read <labs@ollieread.com>
2014-08-02 14:44:50 +0100

    Added fireball ability (requires some randomness removal)

--- src/main/java/com/ollieread/technomagi/ability/active/ActiveAbilityFireball.java
+++ src/main/java/com/ollieread/technomagi/ability/active/ActiveAbilityFireball.java
@@ -1,8 +1,18 @@
 package com.ollieread.technomagi.ability.active;
 
+import java.util.Random;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityFireball;
+import net.minecraft.entity.projectile.EntitySmallFireball;
+import net.minecraft.util.Vec3;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
+
 import com.ollieread.ennds.ability.AbilityActive;
 import com.ollieread.ennds.extended.ExtendedPlayerKnowledge;
 import com.ollieread.technomagi.common.Reference;
+import com.ollieread.technomagi.util.PlayerHelper;
 
 import cpw.mods.fml.common.eventhandler.Event;
 
@@ -23,13 +33,35 @@ public class ActiveAbilityFireball extends AbilityActive
     @Override
     public boolean isAvailable(ExtendedPlayerKnowledge charon)
     {
-        return false;
+        return true;
     }
 
     @Override
     public boolean use(ExtendedPlayerKnowledge charon, Event event)
     {
+        if (event instanceof PlayerInteractEvent) {
+            PlayerInteractEvent interact = (PlayerInteractEvent) event;
+
+            if (interact.action.equals(Action.RIGHT_CLICK_AIR)) {
+                EntityPlayer player = interact.entityPlayer;
+
+                if (!player.worldObj.isRemote) {
+                    Vec3 look = PlayerHelper.getLookVector(player);
+                    Vec3 eye = PlayerHelper.getEyeVector(player);
+                    Random rand = new Random();
+
+                    float f = 2 * 0.5F;
+                    EntityFireball fireball = new EntitySmallFireball(player.worldObj, player, look.xCoord, look.yCoord, look.zCoord);
+                    fireball.posY = eye.yCoord;
+                    fireball.rotationPitch = player.rotationPitch;
+                    fireball.rotationYaw = player.rotationYaw;
+                    player.worldObj.spawnEntityInWorld(fireball);
+                }
+
+                return true;
+            }
+        }
+
         return false;
     }
-
 }
