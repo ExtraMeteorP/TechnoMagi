c6219ad7fdb826695615abc97f367cdfefd629ba
Ollie Read <labs@ollieread.com>
2014-08-09 20:30:19 +0100

    Set it so fireballs now fly straight and cost 15 nanites

--- src/main/java/com/ollieread/technomagi/ability/active/ActiveAbilityFireball.java
+++ src/main/java/com/ollieread/technomagi/ability/active/ActiveAbilityFireball.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityFireball;
 import net.minecraft.entity.projectile.EntitySmallFireball;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.Vec3;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
@@ -45,20 +46,28 @@ public class ActiveAbilityFireball extends AbilityActive
             if (interact.action.equals(Action.RIGHT_CLICK_AIR)) {
                 EntityPlayer player = interact.entityPlayer;
 
-                if (!player.worldObj.isRemote) {
-                    Vec3 look = PlayerHelper.getLookVector(player);
-                    Vec3 eye = PlayerHelper.getEyeVector(player);
-                    Random rand = new Random();
+                if (decreaseNanites(charon, 15)) {
+                    if (!player.worldObj.isRemote) {
+                        Vec3 look = PlayerHelper.getLookVector(player);
+                        Vec3 eye = PlayerHelper.getEyeVector(player);
+                        Random rand = new Random();
 
-                    float f = 2 * 0.5F;
-                    EntityFireball fireball = new EntitySmallFireball(player.worldObj, player, look.xCoord, look.yCoord, look.zCoord);
-                    fireball.posY = eye.yCoord;
-                    fireball.rotationPitch = player.rotationPitch;
-                    fireball.rotationYaw = player.rotationYaw;
-                    player.worldObj.spawnEntityInWorld(fireball);
+                        float f = 2 * 0.5F;
+                        EntityFireball fireball = new EntitySmallFireball(player.worldObj, player, look.xCoord, look.yCoord, look.zCoord);
+                        fireball.posY = eye.yCoord;
+                        fireball.rotationPitch = player.rotationPitch;
+                        fireball.rotationYaw = player.rotationYaw;
+
+                        double d3 = (double) MathHelper.sqrt_double(look.xCoord * look.xCoord + look.yCoord * look.yCoord + look.zCoord * look.zCoord);
+                        fireball.accelerationX = look.xCoord / d3 * 0.1D;
+                        fireball.accelerationY = look.yCoord / d3 * 0.1D;
+                        fireball.accelerationZ = look.zCoord / d3 * 0.1D;
+
+                        player.worldObj.spawnEntityInWorld(fireball);
+                    }
+
+                    return true;
                 }
-
-                return true;
             }
         }
 
