8f251852e9c36c79c48e9c67bdf28f3fcd0fe9b4
Ollie Read <labs@ollieread.com>
2014-08-10 15:09:54 +0100

    Localisation and init tidy up

--- src/main/java/com/ollieread/technomagi/common/init/Knowledge.java
+++ src/main/java/com/ollieread/technomagi/common/init/Knowledge.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.Level;
 
 import com.ollieread.ennds.research.IKnowledge;
 import com.ollieread.ennds.research.IResearchAnalysis;
+import com.ollieread.ennds.research.IResearchEvent;
 import com.ollieread.technomagi.TechnoMagi;
 import com.ollieread.technomagi.research.KnowledgeLightManipulationI;
 import com.ollieread.technomagi.research.KnowledgeLightManipulationII;
@@ -17,6 +18,7 @@ import com.ollieread.technomagi.research.KnowledgeTeleportationI;
 import com.ollieread.technomagi.research.KnowledgeTeleportationII;
 import com.ollieread.technomagi.research.analysis.ResearchAnalysisSpeedPotionI;
 import com.ollieread.technomagi.research.analysis.ResearchAnalysisSpeedPotionII;
+import com.ollieread.technomagi.research.event.ResearchUseEnderpearl;
 
 public class Knowledge
 {
@@ -25,10 +27,12 @@ public class Knowledge
     public static IKnowledge lightManipulationII;
     public static IKnowledge teleportationI;
     public static IKnowledge teleportationII;
-    public static IKnowledge motion;
+    public static IKnowledge motionI;
 
     public static IResearchAnalysis speedPotionI;
     public static IResearchAnalysis speedPotionII;
+
+    public static IResearchEvent useEnderpearl;
 
     public static void init()
     {
@@ -38,9 +42,11 @@ public class Knowledge
         lightManipulationII = new KnowledgeLightManipulationII("lightManipulationII");
         teleportationI = new KnowledgeTeleportationI("teleportationI");
         teleportationII = new KnowledgeTeleportationII("teleportationII");
-        motion = new KnowledgeMotion("motion");
+        motionI = new KnowledgeMotion("motionI");
 
-        speedPotionI = new ResearchAnalysisSpeedPotionI("speedPotionI", motion.getName(), 15, 1, new ArrayList(Arrays.asList(new ItemStack(net.minecraft.init.Items.potionitem, 1, 8194))));
-        speedPotionII = new ResearchAnalysisSpeedPotionII("speedPotionII", motion.getName(), 15, 1, new ArrayList(Arrays.asList(new ItemStack(net.minecraft.init.Items.potionitem, 1, 8226))));
+        speedPotionI = new ResearchAnalysisSpeedPotionI("speedPotionI", motionI.getName(), 15, 1, new ArrayList(Arrays.asList(new ItemStack(net.minecraft.init.Items.potionitem, 1, 8194))));
+        speedPotionII = new ResearchAnalysisSpeedPotionII("speedPotionII", motionI.getName(), 15, 1, new ArrayList(Arrays.asList(new ItemStack(net.minecraft.init.Items.potionitem, 1, 8226))));
+
+        useEnderpearl = new ResearchUseEnderpearl("useEnderpearl", teleportationI.getName(), 10);
     }
 }
