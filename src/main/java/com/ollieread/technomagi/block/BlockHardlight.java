54a03ff7cd96046142fb50d8d2ab354d7775a4b3
Ollie Read <labs@ollieread.com>
2014-09-06 18:13:46 +0100

    Start of metabased hardlight

--- src/main/java/com/ollieread/technomagi/block/BlockHardlight.java
+++ src/main/java/com/ollieread/technomagi/block/BlockHardlight.java
@@ -1,7 +1,12 @@
 package com.ollieread.technomagi.block;
 
+import java.util.List;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.IBlockAccess;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -15,6 +20,16 @@ public class BlockHardlight extends BlockTM
 
         setLightOpacity(0);
         setBlockUnbreakable();
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item item, CreativeTabs tab, List list)
+    {
+        super.getSubBlocks(item, tab, list);
+
+        list.add(new ItemStack(this, 1, 1));
+        list.add(new ItemStack(this, 1, 2));
+        list.add(new ItemStack(this, 1, 3));
     }
 
     public boolean isOpaqueCube()
