bb6df2fcea2481be6d3ce861b2cbdb1d6e1e9fd8
Ollie Read <labs@ollieread.com>
2014-09-06 11:34:41 +0100

    Moved research to the parent knowledge class

--- src/main/java/com/ollieread/technomagi/research/KnowledgeTeleportationI.java
+++ src/main/java/com/ollieread/technomagi/research/KnowledgeTeleportationI.java
@@ -1,14 +1,20 @@
 package com.ollieread.technomagi.research;
 
+import com.ollieread.ennds.research.IResearchEvent;
 import com.ollieread.ennds.research.Knowledge;
 import com.ollieread.technomagi.common.Reference;
+import com.ollieread.technomagi.research.event.ResearchUseEnderpearl;
 
 public class KnowledgeTeleportationI extends Knowledge
 {
 
+    public static IResearchEvent eventUseEnderpearl;
+
     public KnowledgeTeleportationI(String name)
     {
         super(name, Reference.MODID.toLowerCase());
+
+        eventUseEnderpearl = new ResearchUseEnderpearl("useEnderpearl", getName(), 10);
     }
 
 }
