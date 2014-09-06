47bffb8426b0815f91eac076da07d92e12fa2079
Ollie Read <labs@ollieread.com>
2014-09-06 18:13:04 +0100

    Tidy up of Knowledge init

--- src/main/java/com/ollieread/technomagi/common/init/Knowledge.java
+++ src/main/java/com/ollieread/technomagi/common/init/Knowledge.java
@@ -1,24 +1,17 @@
 package com.ollieread.technomagi.common.init;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import net.minecraft.item.ItemStack;
-
 import org.apache.logging.log4j.Level;
 
 import com.ollieread.ennds.research.IKnowledge;
-import com.ollieread.ennds.research.IResearchAnalysis;
-import com.ollieread.ennds.research.IResearchEvent;
 import com.ollieread.technomagi.TechnoMagi;
+import com.ollieread.technomagi.research.KnowledgeHarvestI;
+import com.ollieread.technomagi.research.KnowledgeHarvestII;
+import com.ollieread.technomagi.research.KnowledgeHarvestIII;
 import com.ollieread.technomagi.research.KnowledgeLightManipulationI;
 import com.ollieread.technomagi.research.KnowledgeLightManipulationII;
 import com.ollieread.technomagi.research.KnowledgeMotion;
 import com.ollieread.technomagi.research.KnowledgeTeleportationI;
 import com.ollieread.technomagi.research.KnowledgeTeleportationII;
-import com.ollieread.technomagi.research.analysis.ResearchAnalysisSpeedPotionI;
-import com.ollieread.technomagi.research.analysis.ResearchAnalysisSpeedPotionII;
-import com.ollieread.technomagi.research.event.ResearchUseEnderpearl;
 
 public class Knowledge
 {
@@ -28,11 +21,9 @@ public class Knowledge
     public static IKnowledge teleportationI;
     public static IKnowledge teleportationII;
     public static IKnowledge motionI;
-
-    public static IResearchAnalysis speedPotionI;
-    public static IResearchAnalysis speedPotionII;
-
-    public static IResearchEvent useEnderpearl;
+    public static IKnowledge harvestI;
+    public static IKnowledge harvestII;
+    public static IKnowledge harvestIII;
 
     public static void init()
     {
@@ -43,10 +34,8 @@ public class Knowledge
         teleportationI = new KnowledgeTeleportationI("teleportationI");
         teleportationII = new KnowledgeTeleportationII("teleportationII");
         motionI = new KnowledgeMotion("motionI");
-
-        speedPotionI = new ResearchAnalysisSpeedPotionI("speedPotionI", motionI.getName(), 15, 1, new ArrayList(Arrays.asList(new ItemStack(net.minecraft.init.Items.potionitem, 1, 8194))));
-        speedPotionII = new ResearchAnalysisSpeedPotionII("speedPotionII", motionI.getName(), 15, 1, new ArrayList(Arrays.asList(new ItemStack(net.minecraft.init.Items.potionitem, 1, 8226))));
-
-        useEnderpearl = new ResearchUseEnderpearl("useEnderpearl", teleportationI.getName(), 10);
+        harvestI = new KnowledgeHarvestI("harvestI");
+        harvestII = new KnowledgeHarvestII("harvestII");
+        harvestIII = new KnowledgeHarvestIII("harvestIII");
     }
 }
