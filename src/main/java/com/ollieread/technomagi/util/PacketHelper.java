4a8492300a71ebefdc46f391a382b89ce4aa79ce
Ollie Read <labs@ollieread.com>
2014-09-12 00:50:50 +0100

    Added new method to packet helper for crafter

--- src/main/java/com/ollieread/technomagi/util/PacketHelper.java
+++ src/main/java/com/ollieread/technomagi/util/PacketHelper.java
@@ -1,8 +1,10 @@
 package com.ollieread.technomagi.util;
 
 import com.ollieread.technomagi.network.PacketHandler;
+import com.ollieread.technomagi.network.message.MessageSetCrafting;
 import com.ollieread.technomagi.network.message.MessageSetProgress;
 import com.ollieread.technomagi.network.message.MessageSyncTileEntityTM;
+import com.ollieread.technomagi.tileentity.TileEntityCrafting;
 import com.ollieread.technomagi.tileentity.TileEntityResearch;
 import com.ollieread.technomagi.tileentity.TileEntityTM;
 
@@ -10,6 +12,11 @@ import cpw.mods.fml.common.network.simpleimpl.IMessage;
 
 public class PacketHelper
 {
+
+    public static void setCrafting(TileEntityCrafting machine, boolean progress)
+    {
+        PacketHandler.INSTANCE.sendToServer(new MessageSetCrafting(machine, progress));
+    }
 
     public static void setProgress(TileEntityResearch machine, boolean progress)
     {
