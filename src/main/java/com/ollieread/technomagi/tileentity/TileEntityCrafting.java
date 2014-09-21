84feabe711c7b248f2eb8150a6586754c9c5958d
Ollie Read <labs@ollieread.com>
2014-09-21 21:29:03 +0100

    New textures and crafting effects

--- src/main/java/com/ollieread/technomagi/tileentity/TileEntityCrafting.java
+++ src/main/java/com/ollieread/technomagi/tileentity/TileEntityCrafting.java
@@ -1,5 +1,7 @@
 package com.ollieread.technomagi.tileentity;
 
+import java.util.Random;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
@@ -32,6 +34,18 @@ public class TileEntityCrafting extends TileEntityTM implements IPlayerLocked, I
     protected int waiting;
     protected boolean isCrafting = false;
     public CraftingManager crafting = CraftingManager.getInstance();
+
+    public int field_145926_a;
+    public float field_145933_i;
+    public float field_145931_j;
+    public float field_145932_k;
+    public float field_145929_l;
+    public float field_145930_m;
+    public float field_145927_n;
+    public float field_145928_o;
+    public float field_145925_p;
+    public float field_145924_q;
+    private static Random field_145923_r = new Random();
 
     public TileEntityCrafting()
     {
@@ -81,6 +95,64 @@ public class TileEntityCrafting extends TileEntityTM implements IPlayerLocked, I
     @Override
     public void updateEntity()
     {
+        super.updateEntity();
+        this.field_145927_n = this.field_145930_m;
+        this.field_145925_p = this.field_145928_o;
+        this.field_145924_q += 0.02F;
+        this.field_145930_m -= 0.1F;
+
+        while (this.field_145928_o >= (float) Math.PI) {
+            this.field_145928_o -= ((float) Math.PI * 2F);
+        }
+
+        while (this.field_145928_o < -(float) Math.PI) {
+            this.field_145928_o += ((float) Math.PI * 2F);
+        }
+
+        while (this.field_145924_q >= (float) Math.PI) {
+            this.field_145924_q -= ((float) Math.PI * 2F);
+        }
+
+        while (this.field_145924_q < -(float) Math.PI) {
+            this.field_145924_q += ((float) Math.PI * 2F);
+        }
+
+        float f2;
+
+        for (f2 = this.field_145924_q - this.field_145928_o; f2 >= (float) Math.PI; f2 -= ((float) Math.PI * 2F)) {
+            ;
+        }
+
+        while (f2 < -(float) Math.PI) {
+            f2 += ((float) Math.PI * 2F);
+        }
+
+        this.field_145928_o += f2 * 0.4F;
+
+        if (this.field_145930_m < 0.0F) {
+            this.field_145930_m = 0.0F;
+        }
+
+        if (this.field_145930_m > 1.0F) {
+            this.field_145930_m = 1.0F;
+        }
+
+        ++this.field_145926_a;
+        this.field_145931_j = this.field_145933_i;
+        float f = (this.field_145932_k - this.field_145933_i) * 0.4F;
+        float f3 = 0.2F;
+
+        if (f < -f3) {
+            f = -f3;
+        }
+
+        if (f > f3) {
+            f = f3;
+        }
+
+        this.field_145929_l += (f - this.field_145929_l) * 0.9F;
+        this.field_145933_i += this.field_145929_l;
+
         if (!worldObj.isRemote) {
             if (canCraft() && isCrafting()) {
                 craft();
@@ -121,14 +193,17 @@ public class TileEntityCrafting extends TileEntityTM implements IPlayerLocked, I
 
             if (player != null) {
                 ItemStack recipe = crafting.findMatchingRecipe(inventory, worldObj, player);
-                ItemStack result = inventory.getStackInSlot(9);
 
-                if (result == null) {
-                    return true;
-                }
+                if (recipe != null) {
+                    ItemStack result = inventory.getStackInSlot(9);
 
-                if (result.isItemEqual(recipe) && (result.stackSize + recipe.stackSize) <= result.getMaxStackSize()) {
-                    return true;
+                    if (result == null) {
+                        return true;
+                    }
+
+                    if (result.isItemEqual(recipe) && (result.stackSize + recipe.stackSize) <= result.getMaxStackSize()) {
+                        return true;
+                    }
                 }
             }
         }
@@ -234,6 +309,11 @@ public class TileEntityCrafting extends TileEntityTM implements IPlayerLocked, I
     public void setInventorySlotContents(int i, ItemStack stack)
     {
         inventory.setInventorySlotContents(i, stack);
+    }
+
+    public ItemStack[] getInventory()
+    {
+        return inventory.getInventory();
     }
 
     @Override
