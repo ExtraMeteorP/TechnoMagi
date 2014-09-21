0bca34151ffc299e2b159031b52405b9c43f296a
Ollie Read <labs@ollieread.com>
2014-09-21 21:28:19 +0100

    Fixed text button

--- src/main/java/com/ollieread/technomagi/client/gui/GuiTMTextButton.java
+++ src/main/java/com/ollieread/technomagi/client/gui/GuiTMTextButton.java
@@ -7,10 +7,32 @@ import org.lwjgl.opengl.GL11;
 
 public class GuiTMTextButton extends GuiButton
 {
+    /** The x position of this control. */
+    public int xPosition;
+    /** The y position of this control. */
+    public int yPosition;
+
+    public int xOffset = 0;
+
+    public int yOffset = 0;
 
     public GuiTMTextButton(int id, int x, int y, String s)
     {
         super(id, x, y, 200, 10, s);
+
+        xPosition = x;
+        yPosition = y;
+    }
+
+    public GuiTMTextButton(int id, int x, int y, int xOffset, int yOffset, String s)
+    {
+        super(id, xOffset + x, yOffset + y, 200, 10, s);
+
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+
+        xPosition = x;
+        yPosition = y;
     }
 
     public void drawButton(Minecraft mc, int mx, int my)
@@ -18,7 +40,7 @@ public class GuiTMTextButton extends GuiButton
         if (this.visible) {
             width = mc.fontRenderer.getStringWidth(displayString) + 2;
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-            drawString(mc.fontRenderer, displayString, xPosition + 1, this.yPosition + 1, 16777215);
+            mc.fontRenderer.drawString(displayString, xPosition + 1, this.yPosition + 1, 16777215);
         }
     }
 
