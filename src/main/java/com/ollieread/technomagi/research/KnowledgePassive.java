32f31d376fc01a718d1ee0ab3611eb344e61613b
Ollie Read <labs@ollieread.com>
2014-09-22 18:58:12 +0100

    Passive knowledge progress

--- src/main/java/com/ollieread/technomagi/research/KnowledgePassive.java
+++ src/main/java/com/ollieread/technomagi/research/KnowledgePassive.java
@@ -1,14 +1,29 @@
 package com.ollieread.technomagi.research;
 
+import com.ollieread.ennds.research.IResearchEvent;
 import com.ollieread.ennds.research.Knowledge;
 import com.ollieread.technomagi.common.Reference;
+import com.ollieread.technomagi.research.event.ResearchEventKnowledgeChicken;
+import com.ollieread.technomagi.research.event.ResearchEventKnowledgeCow;
+import com.ollieread.technomagi.research.event.ResearchEventKnowledgePig;
+import com.ollieread.technomagi.research.event.ResearchEventKnowledgeSheep;
 
 public class KnowledgePassive extends Knowledge
 {
 
+    public static IResearchEvent pig;
+    public static IResearchEvent sheep;
+    public static IResearchEvent cow;
+    public static IResearchEvent chicken;
+
     public KnowledgePassive(String name)
     {
         super(name, Reference.MODID.toLowerCase());
+
+        pig = new ResearchEventKnowledgePig("knowledgePig", this.getName(), 25);
+        sheep = new ResearchEventKnowledgeSheep("knowledgeSheep", this.getName(), 25);
+        cow = new ResearchEventKnowledgeCow("knowledgeCow", this.getName(), 25);
+        chicken = new ResearchEventKnowledgeChicken("knowledgeChicken", this.getName(), 25);
     }
 
 }
---
+++ src/main/java/com/ollieread/technomagi/research/event/ResearchEventKnowledgeChicken.java
@@ -0,0 +1,40 @@
+package com.ollieread.technomagi.research.event;
+
+import com.ollieread.ennds.extended.ExtendedPlayerKnowledge;
+import com.ollieread.ennds.research.IResearchEvent;
+import com.ollieread.ennds.research.Research;
+import com.ollieread.technomagi.common.Reference;
+
+public class ResearchEventKnowledgeChicken extends Research implements IResearchEvent
+{
+
+    public ResearchEventKnowledgeChicken(String name, String knowledge, int progress)
+    {
+        super(name, knowledge, progress, Reference.MODID.toLowerCase());
+    }
+
+    @Override
+    public boolean isRepeating()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean canPerform(ExtendedPlayerKnowledge charon)
+    {
+        return true;
+    }
+
+    @Override
+    public String getEvent()
+    {
+        return "knowledgeEntityChicken";
+    }
+
+    @Override
+    public int getChance()
+    {
+        return 1;
+    }
+
+}
