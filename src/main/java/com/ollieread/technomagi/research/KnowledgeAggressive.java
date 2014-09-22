c888fe87c10635f2b83bfc81eb0e4b811617c1d4
Ollie Read <labs@ollieread.com>
2014-09-22 18:58:55 +0100

    Aggressive knowledge progress

--- src/main/java/com/ollieread/technomagi/research/KnowledgeAggressive.java
+++ src/main/java/com/ollieread/technomagi/research/KnowledgeAggressive.java
@@ -1,14 +1,29 @@
 package com.ollieread.technomagi.research;
 
+import com.ollieread.ennds.research.IResearchEvent;
 import com.ollieread.ennds.research.Knowledge;
 import com.ollieread.technomagi.common.Reference;
+import com.ollieread.technomagi.research.event.ResearchEventKnowledgeCreeper;
+import com.ollieread.technomagi.research.event.ResearchEventKnowledgeEnderman;
+import com.ollieread.technomagi.research.event.ResearchEventKnowledgeSkeleton;
+import com.ollieread.technomagi.research.event.ResearchEventKnowledgeZombie;
 
 public class KnowledgeAggressive extends Knowledge
 {
 
+    public static IResearchEvent creeper;
+    public static IResearchEvent enderman;
+    public static IResearchEvent skeleton;
+    public static IResearchEvent zombie;
+
     public KnowledgeAggressive(String name)
     {
         super(name, Reference.MODID.toLowerCase());
+
+        creeper = new ResearchEventKnowledgeCreeper("knowledgeCreeper", this.getName(), 25);
+        enderman = new ResearchEventKnowledgeEnderman("knowledgeEnderman", this.getName(), 25);
+        skeleton = new ResearchEventKnowledgeSkeleton("knowledgeSkeleton", this.getName(), 25);
+        zombie = new ResearchEventKnowledgeZombie("knowledgeZombie", this.getName(), 25);
     }
 
 }
---
+++ src/main/java/com/ollieread/technomagi/research/event/ResearchEventKnowledgeCreeper.java
@@ -0,0 +1,40 @@
+package com.ollieread.technomagi.research.event;
+
+import com.ollieread.ennds.extended.ExtendedPlayerKnowledge;
+import com.ollieread.ennds.research.IResearchEvent;
+import com.ollieread.ennds.research.Research;
+import com.ollieread.technomagi.common.Reference;
+
+public class ResearchEventKnowledgeCreeper extends Research implements IResearchEvent
+{
+
+    public ResearchEventKnowledgeCreeper(String name, String knowledge, int progress)
+    {
+        super(name, knowledge, progress, Reference.MODID.toLowerCase());
+    }
+
+    @Override
+    public boolean isRepeating()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean canPerform(ExtendedPlayerKnowledge charon)
+    {
+        return true;
+    }
+
+    @Override
+    public String getEvent()
+    {
+        return "knowledgeEntityCreeper";
+    }
+
+    @Override
+    public int getChance()
+    {
+        return 1;
+    }
+
+}
