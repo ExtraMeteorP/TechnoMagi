74241067ad75a8fa7604fcccbee6a7cb3648fc55
Ollie Read <labs@ollieread.com>
2014-09-22 01:18:01 +0100

    New items :D

---
+++ src/main/java/com/ollieread/technomagi/item/ItemVoidCrystal.java
@@ -0,0 +1,11 @@
+package com.ollieread.technomagi.item;
+
+public class ItemVoidCrystal extends ItemTM
+{
+
+    public ItemVoidCrystal(String name)
+    {
+        super(name);
+    }
+
+}
---
+++ src/main/java/com/ollieread/technomagi/item/ItemVoidIngot.java
@@ -0,0 +1,11 @@
+package com.ollieread.technomagi.item;
+
+public class ItemVoidIngot extends ItemTM
+{
+
+    public ItemVoidIngot(String name)
+    {
+        super(name);
+    }
+
+}
