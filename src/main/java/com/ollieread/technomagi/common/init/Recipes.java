290b192faabeac37609fcbd3040b7b3235276792
Ollie Read <labs@ollieread.com>
2014-09-22 01:16:03 +0100

    Made certain recipes require knowledge

--- src/main/java/com/ollieread/technomagi/common/init/Recipes.java
+++ src/main/java/com/ollieread/technomagi/common/init/Recipes.java
@@ -50,23 +50,23 @@ public class Recipes
         RecipeHelper.addConstructRecipe(Blocks.blockNaniteReplicator, new ItemStack[] { stackNaniteContainer4, new ItemStack(Items.itemComponent, 2, 5), stackSampleVile8, new ItemStack(net.minecraft.init.Items.redstone, 4) });
 
         // teleporter
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Blocks.blockTeleporter, 1, 0), " w ", " y ", " z ", 'y', new ItemStack(Items.itemUnit, 1, 3), 'z', new ItemStack(Blocks.blockConstruct, 1), 'w', new ItemStack(net.minecraft.init.Blocks.piston, 1));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Blocks.blockTeleporter, 1, 1), " w ", " y ", " z ", 'y', new ItemStack(Items.itemUnit, 1, 3), 'z', new ItemStack(Blocks.blockConstruct, 1), 'w', new ItemStack(net.minecraft.init.Blocks.sticky_piston, 1));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Blocks.blockTeleporter, 1, 2), " w ", " y ", " z ", 'y', new ItemStack(Items.itemUnit, 1, 4), 'z', new ItemStack(Blocks.blockConstruct, 1), 'w', new ItemStack(net.minecraft.init.Items.ender_pearl, 1));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Blocks.blockTeleporter, 1, 3), " w ", " y ", " z ", 'y', new ItemStack(Items.itemUnit, 1, 5), 'z', new ItemStack(Blocks.blockConstruct, 1), 'w', new ItemStack(net.minecraft.init.Items.emerald, 1));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeTeleportation.getName(), new ItemStack(Blocks.blockTeleporter, 1, 0), " w ", " y ", " z ", 'y', new ItemStack(Items.itemUnit, 1, 3), 'z', new ItemStack(Blocks.blockConstruct, 1), 'w', new ItemStack(net.minecraft.init.Blocks.piston, 1));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeTeleportation.getName(), new ItemStack(Blocks.blockTeleporter, 1, 1), " w ", " y ", " z ", 'y', new ItemStack(Items.itemUnit, 1, 3), 'z', new ItemStack(Blocks.blockConstruct, 1), 'w', new ItemStack(net.minecraft.init.Blocks.sticky_piston, 1));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeTeleportation.getName(), new ItemStack(Blocks.blockTeleporter, 1, 2), " w ", " y ", " z ", 'y', new ItemStack(Items.itemUnit, 1, 4), 'z', new ItemStack(Blocks.blockConstruct, 1), 'w', new ItemStack(net.minecraft.init.Items.ender_pearl, 1));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeTeleportation.getName(), new ItemStack(Blocks.blockTeleporter, 1, 3), " w ", " y ", " z ", 'y', new ItemStack(Items.itemUnit, 1, 5), 'z', new ItemStack(Blocks.blockConstruct, 1), 'w', new ItemStack(net.minecraft.init.Items.emerald, 1));
 
         // life unit
         RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 0), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.egg, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
         RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 1), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 0));
         RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 2), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 1));
         // teleport unit
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 3), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.ender_pearl, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 4), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 3));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 5), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 4));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeTeleportation.getName(), new ItemStack(Items.itemUnit, 1, 3), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.ender_pearl, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeTeleportation.getName(), new ItemStack(Items.itemUnit, 1, 4), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 3));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeTeleportation.getName(), new ItemStack(Items.itemUnit, 1, 5), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 4));
         // subspace unit
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 6), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.ender_pearl, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 7), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 6));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 8), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 7));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeSubspace.getName(), new ItemStack(Items.itemUnit, 1, 6), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.ender_pearl, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeSubspace.getName(), new ItemStack(Items.itemUnit, 1, 7), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 6));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeSubspace.getName(), new ItemStack(Items.itemUnit, 1, 8), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 7));
         // nanite unit
         RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 9), "xzx", " y ", "x x", 'x', ironIngot, 'y', stackNaniteContainer, 'z', new ItemStack(Items.itemComponent, 1, 5));
         RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 10), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 9));
@@ -76,26 +76,26 @@ public class Recipes
         RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 13), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 12));
         RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 14), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 13));
         // exo unit
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 15), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.blaze_powder, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 16), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 15));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 17), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 16));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeExothermic.getName(), new ItemStack(Items.itemUnit, 1, 15), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.blaze_powder, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeExothermic.getName(), new ItemStack(Items.itemUnit, 1, 16), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 15));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeExothermic.getName(), new ItemStack(Items.itemUnit, 1, 17), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 16));
         // endo unit
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 18), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.snowball, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 19), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 18));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 20), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 19));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeEndothermic.getName(), new ItemStack(Items.itemUnit, 1, 18), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.snowball, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeEndothermic.getName(), new ItemStack(Items.itemUnit, 1, 19), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 18));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeEndothermic.getName(), new ItemStack(Items.itemUnit, 1, 20), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 19));
         // force unit
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 21), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Blocks.gravel, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 22), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 21));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 23), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 22));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeForce.getName(), new ItemStack(Items.itemUnit, 1, 21), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Blocks.gravel, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeForce.getName(), new ItemStack(Items.itemUnit, 1, 22), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 21));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeForce.getName(), new ItemStack(Items.itemUnit, 1, 23), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 22));
         // void unit
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 24), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.skull, 1, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 25), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 24));
-        RecipeHelper.addShapedRecipe(null, new ItemStack(Items.itemUnit, 1, 26), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 25));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeVoid.getName(), new ItemStack(Items.itemUnit, 1, 24), "xzx", " y ", "x x", 'x', ironIngot, 'y', new ItemStack(net.minecraft.init.Items.skull, 1, 1), 'z', new ItemStack(Items.itemComponent, 1, 5));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeVoid.getName(), new ItemStack(Items.itemUnit, 1, 25), "x x", " y ", "x x", 'x', goldIngot, 'y', new ItemStack(Items.itemUnit, 1, 24));
+        RecipeHelper.addShapedRecipe(Knowledge.knowledgeVoid.getName(), new ItemStack(Items.itemUnit, 1, 26), "x x", " y ", "x x", 'x', diamond, 'y', new ItemStack(Items.itemUnit, 1, 25));
 
         // Exothermic
-        RecipeHelper.addExothermicRecipe(new ItemStack(net.minecraft.init.Blocks.log), new ItemStack(net.minecraft.init.Items.coal, 1, 1), 250);
+        RecipeHelper.addExothermicRecipe(new ItemStack(net.minecraft.init.Blocks.log), new ItemStack(net.minecraft.init.Items.coal, 1, 1), 500);
         // Endothermic
-        RecipeHelper.addEndothermicRecipe(new ItemStack(net.minecraft.init.Blocks.cobblestone), new ItemStack(net.minecraft.init.Blocks.stone, 1), 250);
+        RecipeHelper.addEndothermicRecipe(new ItemStack(net.minecraft.init.Blocks.cobblestone), new ItemStack(net.minecraft.init.Blocks.stone, 1), 500);
     }
 
 }
