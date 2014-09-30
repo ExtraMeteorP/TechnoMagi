d2b3fc11f7682fe8cbcf09208374f5fb24f9818d
Ollie Read <labs@ollieread.com>
2014-09-30 20:18:26 +0100

    Project knowledge progress

--- src/main/java/com/ollieread/technomagi/knowledge/KnowledgeProjectile.java
+++ src/main/java/com/ollieread/technomagi/knowledge/KnowledgeProjectile.java
@@ -1,14 +1,31 @@
 package com.ollieread.technomagi.knowledge;
 
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+import com.ollieread.ennds.research.IResearchCrafting;
+import com.ollieread.ennds.research.IResearchEvent;
 import com.ollieread.ennds.research.Knowledge;
 import com.ollieread.technomagi.common.Reference;
+import com.ollieread.technomagi.knowledge.research.ResearchCrafting;
+import com.ollieread.technomagi.knowledge.research.ResearchEvent;
 
 public class KnowledgeProjectile extends Knowledge
 {
 
+    public static IResearchEvent damageArrow;
+    public static IResearchEvent fireArrow;
+    public static IResearchCrafting cook;
+    public static IResearchEvent eat;
+
     public KnowledgeProjectile(String name)
     {
         super(name, Reference.MODID.toLowerCase());
+
+        damageArrow = new ResearchEvent("damageArrow", getName(), 25, "damageArrow", false, 2, null);
+        fireArrow = new ResearchEvent("fireArrow", getName(), 25, "fireArrow", false, 2, null);
+        cook = new ResearchCrafting("cookChicken", getName(), 25, new ItemStack(Items.cooked_chicken), false, 2, null);
+        eat = new ResearchEvent("eatChicken", getName(), 25, "eatChicken", false, 2, null);
     }
 
 }
