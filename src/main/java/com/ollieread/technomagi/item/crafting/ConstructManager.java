3afbfbf3b893e7b2ec69b616fe63ef48d693824d
Ollie Read <labs@ollieread.com>
2014-09-30 20:14:58 +0100

    Added support for knowledge in the construct manager

--- src/main/java/com/ollieread/technomagi/item/crafting/ConstructManager.java
+++ src/main/java/com/ollieread/technomagi/item/crafting/ConstructManager.java
@@ -7,20 +7,24 @@ import java.util.List;
 import java.util.Map;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+
+import com.ollieread.ennds.extended.ExtendedPlayerKnowledge;
 
 public class ConstructManager
 {
 
     private static final ConstructManager instance = new ConstructManager();
     private Map<Block, List<ItemStack>> recipes = new LinkedHashMap<Block, List<ItemStack>>();
+    private Map<Block, String> recipeKnowledge = new LinkedHashMap<Block, String>();
 
     public static final ConstructManager getInstance()
     {
         return instance;
     }
 
-    public void addRecipe(Block block, ItemStack[] stacks)
+    public void addRecipe(Block block, ItemStack[] stacks, String knowledge)
     {
         if (block != null && stacks != null && !recipes.containsKey(block)) {
             List list = new ArrayList();
@@ -30,36 +34,47 @@ public class ConstructManager
             }
 
             recipes.put(block, list);
+
+            if (knowledge != null) {
+                recipeKnowledge.put(block, knowledge);
+            }
         }
     }
 
-    public Block findMatchingRecipe(ItemStack[] stacks)
+    public Block findMatchingRecipe(ItemStack[] stacks, EntityPlayer player)
     {
         if (stacks != null && stacks.length > 0) {
-            for (Iterator<Block> i = recipes.keySet().iterator(); i.hasNext();) {
-                Block block = i.next();
-                List<ItemStack> recipe = recipes.get(block);
+            ExtendedPlayerKnowledge playerKnowledge = ExtendedPlayerKnowledge.get(player);
 
-                if (recipe.size() > 0) {
-                    if (recipe.size() == stacks.length) {
-                        int m = recipe.size();
+            if (playerKnowledge != null && !playerKnowledge.canSpecialise()) {
+                for (Iterator<Block> i = recipes.keySet().iterator(); i.hasNext();) {
+                    Block block = i.next();
+                    List<ItemStack> recipe = recipes.get(block);
+                    String knowledge = recipeKnowledge.get(block);
 
-                        for (int x = 0; x < stacks.length; x++) {
-                            if (stacks[x] != null) {
-                                for (Iterator<ItemStack> it = recipe.iterator(); it.hasNext();) {
-                                    ItemStack stack = it.next();
+                    if (knowledge == null || playerKnowledge.hasKnowledge(knowledge)) {
+                        if (recipe.size() > 0) {
+                            if (recipe.size() == stacks.length) {
+                                int m = recipe.size();
 
-                                    if (stacks[x].isItemEqual(stack) && stacks[x].stackSize == stack.stackSize) {
-                                        m--;
+                                for (int x = 0; x < stacks.length; x++) {
+                                    if (stacks[x] != null) {
+                                        for (Iterator<ItemStack> it = recipe.iterator(); it.hasNext();) {
+                                            ItemStack stack = it.next();
 
-                                        break;
+                                            if (stacks[x].isItemEqual(stack) && stacks[x].stackSize == stack.stackSize) {
+                                                m--;
+
+                                                break;
+                                            }
+                                        }
                                     }
                                 }
-                            }
-                        }
 
-                        if (m == 0) {
-                            return block;
+                                if (m == 0) {
+                                    return block;
+                                }
+                            }
                         }
                     }
                 }
