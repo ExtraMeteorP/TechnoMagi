9108e694e2827cb5468f0f89e4382bf30fd8a134
Ollie Read <labs@ollieread.com>
2014-11-02 16:59:39 +0000

    Renamed reactive crafting ability

--- src/main/java/com/ollieread/technomagi/common/init/Abilities.java
+++ src/main/java/com/ollieread/technomagi/common/init/Abilities.java
@@ -9,10 +9,9 @@ import org.apache.logging.log4j.Level;
 import com.ollieread.ennds.ability.IAbilityActive;
 import com.ollieread.ennds.ability.IAbilityPassive;
 import com.ollieread.technomagi.TechnoMagi;
-import com.ollieread.technomagi.ability.active.ActiveAbilityEndothermic;
-import com.ollieread.technomagi.ability.active.ActiveAbilityExothermic;
 import com.ollieread.technomagi.ability.active.ActiveAbilityProjectile;
 import com.ollieread.technomagi.ability.active.ActiveAbilityProjectileExothermic;
+import com.ollieread.technomagi.ability.active.ActiveAbilityReactive;
 import com.ollieread.technomagi.ability.passive.PassiveAbilityNanites;
 
 public class Abilities
@@ -20,7 +19,7 @@ public class Abilities
 
     public static Map<String, Boolean> enabledAbilities;
 
-    public static IAbilityActive abilityActiveExothermic;
+    public static IAbilityActive abilityActiveReactive;
     public static IAbilityActive abilityActiveEndothermic;
     public static IAbilityActive abilityActiveFire;
     public static IAbilityActive abilityActiveFireball;
@@ -47,11 +46,8 @@ public class Abilities
 
         TechnoMagi.logger.log(Level.INFO, "Initiating & registering abilities");
 
-        if (config.getBoolean("exothermic", "Ability States", true, null)) {
-            abilityActiveExothermic = new ActiveAbilityExothermic("exothermic", config.getInt("exothermic", "Ability Cost", 10, 1, 100, null));
-        }
-        if (config.getBoolean("endothermic", "Ability States", true, null)) {
-            abilityActiveEndothermic = new ActiveAbilityEndothermic("endothermic", config.getInt("endothermic", "Ability Cost", 10, 1, 100, null));
+        if (config.getBoolean("reactive", "Ability States", true, null)) {
+            abilityActiveReactive = new ActiveAbilityReactive("reactive", config.getInt("reactive", "Ability Cost", 10, 1, 100, null));
         }
         if (config.getBoolean("projectile", "Ability States", true, null)) {
             abilityActiveProjectile = new ActiveAbilityProjectile("projectile", config.getInt("projectile", "Ability Cost", 5, 1, 100, null));
