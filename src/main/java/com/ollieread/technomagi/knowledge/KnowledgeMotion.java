717598a2cd851dc1bf2d88fe88a2b39bffd6dd44
Ollie Read <labs@ollieread.com>
2014-12-02 20:22:06 +0000

    Changed research for forge, motion and projectile

--- src/main/java/com/ollieread/technomagi/knowledge/KnowledgeMotion.java
+++ src/main/java/com/ollieread/technomagi/knowledge/KnowledgeMotion.java
@@ -15,8 +15,8 @@ public class KnowledgeMotion extends Knowledge
 
     public static IResearchAnalysis sugar;
     public static IResearchAnalysis potionSwiftness;
-    public static IResearchAnalysis sugarSpiderEye;
     public static IResearchAnalysis potionSlowness;
+    public static IResearchAnalysis sugarSpiderEye;
 
     public KnowledgeMotion(String name)
     {
@@ -24,8 +24,8 @@ public class KnowledgeMotion extends Knowledge
 
         sugar = new ResearchAnalysis("analyseSugarMotion", getName(), 25, Arrays.asList(new ItemStack[] { new ItemStack(Items.sugar) }), false, 3, new String[] { "force" });
         potionSwiftness = new ResearchAnalysis("analyseSwiftnessMotion", getName(), 25, Arrays.asList(new ItemStack[] { new ItemStack(Items.potionitem, 1, 8194) }), false, 2, new String[] { "force" });
-        sugarSpiderEye = new ResearchAnalysis("analyseSugarSpiderEyeMotion", getName(), 25, Arrays.asList(new ItemStack[] { new ItemStack(Items.sugar), new ItemStack(Items.fermented_spider_eye) }), false, 3, new String[] { "force" });
         potionSlowness = new ResearchAnalysis("analyseSlownessMotion", getName(), 25, Arrays.asList(new ItemStack[] { new ItemStack(Items.potionitem, 1, 8202) }), false, 2, new String[] { "force" });
+        sugarSpiderEye = new ResearchAnalysis("analyseSugarSpiderEyeMotion", getName(), 25, Arrays.asList(new ItemStack[] { new ItemStack(Items.sugar), new ItemStack(Items.fermented_spider_eye) }), false, 3, new String[] { "force" });
     }
 
 }
--- src/main/java/com/ollieread/technomagi/knowledge/KnowledgeProjectile.java
+++ src/main/java/com/ollieread/technomagi/knowledge/KnowledgeProjectile.java
@@ -1,5 +1,6 @@
 package com.ollieread.technomagi.knowledge;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
@@ -15,17 +16,20 @@ public class KnowledgeProjectile extends Knowledge
 
     public static IResearchEvent damageArrow;
     public static IResearchEvent fireArrow;
-    public static IResearchCrafting cook;
-    public static IResearchEvent eat;
+    public static IResearchEvent useEgg;
+    public static IResearchEvent useSnowball;
+    public static IResearchCrafting craftArrow;
+    public static IResearchCrafting craftDispenser;
 
     public KnowledgeProjectile(String name)
     {
         super(name, Reference.MODID.toLowerCase());
 
-        damageArrow = new ResearchEvent("damageArrow", getName(), 25, "damageArrow", false, 2, null);
-        fireArrow = new ResearchEvent("fireArrow", getName(), 25, "fireArrow", false, 2, null);
-        cook = new ResearchCrafting("cookChicken", getName(), 25, new ItemStack(Items.cooked_chicken), false, 2, null);
-        eat = new ResearchEvent("eatChicken", getName(), 25, "eatChicken", false, 2, null);
+        damageArrow = new ResearchEvent("damageArrow", getName(), 20, "damageArrow", false, 2, new String[] { "motion" });
+        fireArrow = new ResearchEvent("fireArrow", getName(), 20, "fireArrow", false, 2, new String[] { "motion" });
+        craftArrow = new ResearchCrafting("craftArrow", getName(), 20, new ItemStack(Items.arrow), false, 2, new String[] { "motion" });
+        useEgg = new ResearchEvent("useEgg", getName(), 20, "useEgg", false, 2, new String[] { "motion" });
+        useSnowball = new ResearchEvent("useSnowball", getName(), 20, "useSnowball", false, 2, new String[] { "motion" });
+        craftDispenser = new ResearchCrafting("craftDispenser", getName(), 20, new ItemStack(Blocks.dispenser), false, 2, new String[] { "motion" });
     }
-
 }
