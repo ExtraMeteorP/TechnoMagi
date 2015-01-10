928e17630f72a30b30b3d39aeceb4a7f90201a9d
Ollie Read <labs@ollieread.com>
2015-01-10 16:36:24 +0000

    Added in check for potionTypes length

--- src/main/java/com/ollieread/technomagi/common/init/Potions.java
+++ src/main/java/com/ollieread/technomagi/common/init/Potions.java
@@ -1,5 +1,10 @@
 package com.ollieread.technomagi.common.init;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import net.minecraft.potion.Potion;
+
 import org.apache.logging.log4j.Level;
 
 import com.ollieread.technomagi.TechnoMagi;
@@ -13,6 +18,30 @@ public class Potions
     public static void init()
     {
         TechnoMagi.logger.log(Level.INFO, "Initiating & registering potions");
+
+        if (Potion.potionTypes.length < 256) {
+            Potion[] potionTypes = null;
+
+            for (Field f : Potion.class.getDeclaredFields()) {
+                f.setAccessible(true);
+
+                try {
+                    if (f.getName().equals("potionTypes") || f.getName().equals("field_76425_a")) {
+                        Field modfield = Field.class.getDeclaredField("modifiers");
+                        modfield.setAccessible(true);
+                        modfield.setInt(f, f.getModifiers() & ~Modifier.FINAL);
+
+                        potionTypes = (Potion[]) f.get(null);
+                        final Potion[] newPotionType = new Potion[256];
+                        System.arraycopy(potionTypes, 0, newPotionType, 0, potionTypes.length);
+                        f.set(null, newPotionType);
+                    }
+                } catch (Exception e) {
+                    System.err.println("Severe error, please report this to the mod author");
+                    System.err.println(e);
+                }
+            }
+        }
 
         // potionHardness = new PotionTM(30, "potionHardness", false, 0);
     }
