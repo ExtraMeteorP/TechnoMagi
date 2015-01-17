592cf2fd0ad9e267beffb01c64def474d4ce4579
Ollie Read <labs@ollieread.com>
2015-01-17 23:08:18 +0000

    Added swine knowledge

--- src/main/java/com/ollieread/technomagi/knowledge/biology/KnowledgeSwine.java
+++ src/main/java/com/ollieread/technomagi/knowledge/biology/KnowledgeSwine.java
@@ -3,30 +3,46 @@
  */
 package com.ollieread.technomagi.knowledge.biology;
 
+import net.minecraft.entity.passive.EntityPig;
+
+import com.ollieread.ennds.research.IResearch;
 import com.ollieread.ennds.research.Knowledge;
+import com.ollieread.ennds.research.ShapelessAnalysisRecipe;
+import com.ollieread.technomagi.common.Information;
+import com.ollieread.technomagi.common.Reference;
+import com.ollieread.technomagi.research.ResearchAnalysis;
+import com.ollieread.technomagi.research.ResearchEvent;
+import com.ollieread.technomagi.util.EventHelper;
+import com.ollieread.technomagi.util.ItemHelper;
 
 /**
- * @author ollie
+ * @author ollieread
  *
  */
 public class KnowledgeSwine extends Knowledge
 {
 
-    /**
-     * @param name
-     * @param Modid
-     * @param knowledge
-     * @param category
-     */
-    public KnowledgeSwine(String name, String Modid, String[] knowledge, String category)
+    public static IResearch attacked;
+    public static IResearch killed;
+    public static IResearch analysePork;
+    public static IResearch saddled;
+    public static IResearch ridden;
+
+    public KnowledgeSwine(String name, String[] knowledge, String category)
     {
-        super(name, Modid, knowledge, category);
+        super(name, Reference.MODID.toLowerCase(), knowledge, category);
+
+        attacked = new ResearchEvent("attacked", getName(), EventHelper.entityAttacked(EntityPig.class)).setProgress(5).setChance(5).setRepeatition(3);
+        killed = new ResearchEvent("killed", getName(), EventHelper.entityKilled(EntityPig.class)).setProgress(15).setChance(6);
+        saddled = new ResearchEvent("saddled", getName(), "saddledPig").setProgress(4).setChance(6).setRepeatition(5);
+        analysePork = new ResearchAnalysis("analysePork", getName(), new ShapelessAnalysisRecipe(ItemHelper.item("pork"))).setProgress(15).setChance(6);
+        ridden = new ResearchEvent("ridingPig", getName(), "ridingPig").setProgress(5).setChance(6).setRepeatition(7);
     }
 
     @Override
     public String[] getIntrigue()
     {
-        return null;
+        return Information.getInformation("intrigue", getName());
     }
 
 }
