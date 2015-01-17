81a388fd320b69b7836886cb66e7b50c8850df65
Ollie Read <labs@ollieread.com>
2015-01-17 16:25:47 +0000

    Recipes automatically added for reactive crafting

--- src/main/java/com/ollieread/technomagi/common/init/Recipes.java
+++ src/main/java/com/ollieread/technomagi/common/init/Recipes.java
@@ -1,17 +1,31 @@
 package com.ollieread.technomagi.common.init;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
-import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
+
+import com.ollieread.technomagi.TechnoMagi;
+import com.ollieread.technomagi.item.crafting.RecipeManager;
 
 public class Recipes
 {
 
-    public static List<IRecipe> recipes;
-
     public static void init()
     {
-        recipes = new ArrayList<IRecipe>();
+        TechnoMagi.info("Initiating & registering recipes");
+
+        Map<ItemStack, ItemStack> smeltingList = FurnaceRecipes.smelting().getSmeltingList();
+
+        for (Iterator<Entry<ItemStack, ItemStack>> i = smeltingList.entrySet().iterator(); i.hasNext();) {
+            Entry<ItemStack, ItemStack> entry = i.next();
+
+            if (Block.getBlockFromItem(entry.getKey().getItem()) != null) {
+                RecipeManager.reactive.add(entry.getKey(), entry.getValue(), 200);
+            }
+        }
     }
 }
