31f2e8e5501ea365ceca0dd8b3249d52f053018f
Ollie Read <labs@ollieread.com>
2015-01-18 01:38:10 +0000

    Added perception knowledge

--- src/main/java/com/ollieread/technomagi/knowledge/biology/KnowledgePerception.java
+++ src/main/java/com/ollieread/technomagi/knowledge/biology/KnowledgePerception.java
@@ -3,30 +3,64 @@
  */
 package com.ollieread.technomagi.knowledge.biology;
 
+import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.monster.EntityIronGolem;
+import net.minecraft.entity.monster.EntityPigZombie;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.passive.EntityOcelot;
+import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.passive.EntityWolf;
+
+import com.ollieread.ennds.research.IResearch;
 import com.ollieread.ennds.research.Knowledge;
+import com.ollieread.technomagi.common.Information;
+import com.ollieread.technomagi.common.Reference;
+import com.ollieread.technomagi.research.ResearchEvent;
+import com.ollieread.technomagi.util.EventHelper;
 
 /**
- * @author ollie
+ * @author ollieread
  *
  */
 public class KnowledgePerception extends Knowledge
 {
 
-    /**
-     * @param name
-     * @param Modid
-     * @param knowledge
-     * @param category
-     */
-    public KnowledgePerception(String name, String Modid, String[] knowledge, String category)
+    public static IResearch targetedByPigmen;
+    public static IResearch targetedByEnderman;
+    public static IResearch targetedByWolf;
+    public static IResearch targetedByCow;
+    public static IResearch targetedByPig;
+    public static IResearch targetedBySheep;
+    public static IResearch zombieTargetsVillager;
+    public static IResearch ironGolemTargetsZombie;
+    public static IResearch wolfTargetsSheep;
+    public static IResearch ocelotTargetsChicken;
+
+    public KnowledgePerception(String name, String[] knowledge, String category)
     {
-        super(name, Modid, knowledge, category);
+        super(name, Reference.MODID.toLowerCase(), knowledge, category);
+
+        targetedByPigmen = new ResearchEvent("targetedByPigmen", getName(), EventHelper.entityTargeted(EntityPigZombie.class)).setProgress(3).setChance(6).setRepeatition(4);
+        targetedByEnderman = new ResearchEvent("targetedByEnderman", getName(), EventHelper.entityTargeted(EntityEnderman.class)).setProgress(3).setChance(6).setRepeatition(4);
+        targetedByWolf = new ResearchEvent("targetedByWolf", getName(), EventHelper.entityTargeted(EntityWolf.class)).setProgress(3).setChance(6).setRepeatition(4);
+        targetedByCow = new ResearchEvent("targetedByCow", getName(), EventHelper.entityTargeted(EntityCow.class)).setProgress(3).setChance(6).setRepeatition(4);
+        targetedByPig = new ResearchEvent("targetedByPig", getName(), EventHelper.entityTargeted(EntityPig.class)).setProgress(3).setChance(6).setRepeatition(4);
+        targetedBySheep = new ResearchEvent("targetedBySheep", getName(), EventHelper.entityTargeted(EntitySheep.class)).setProgress(3).setChance(6).setRepeatition(4);
+
+        zombieTargetsVillager = new ResearchEvent("zombieTargetsVillager", getName(), EventHelper.entityTargeted(EntityZombie.class, EntityVillager.class)).setProgress(5).setChance(2).setRepeatition(2);
+        ironGolemTargetsZombie = new ResearchEvent("ironGolemTargetsZombie", getName(), EventHelper.entityTargeted(EntityIronGolem.class, EntityZombie.class)).setProgress(5).setChance(2).setRepeatition(2);
+        wolfTargetsSheep = new ResearchEvent("wolfTargetsSheep", getName(), EventHelper.entityTargeted(EntityWolf.class, EntitySheep.class)).setProgress(5).setChance(2).setRepeatition(2);
+        ocelotTargetsChicken = new ResearchEvent("ocelotTargetsChicken", getName(), EventHelper.entityTargeted(EntityOcelot.class, EntityChicken.class)).setProgress(5).setChance(2).setRepeatition(2);
     }
 
     @Override
     public String[] getIntrigue()
     {
-        return null;
+        return Information.getInformation("intrigue", getName());
     }
 
 }
