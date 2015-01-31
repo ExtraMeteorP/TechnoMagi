df8bb33d50dc94566a2ad8857453f012c43f39ac
Ollie Read <labs@ollieread.com>
2015-01-31 07:48:27 +0000

    Added basic blocks

--- src/main/java/com/ollieread/technomagi/common/blocks/BlockBase.java
+++ src/main/java/com/ollieread/technomagi/common/blocks/BlockBase.java
@@ -1,6 +1,16 @@
 package com.ollieread.technomagi.common.blocks;
 
-public class BlockBase
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockBase extends Block
 {
+
+    protected BlockBase(String name, Material material)
+    {
+        super(material);
+
+        this.setBlockName(name);
+    }
 
 }
--- src/main/java/com/ollieread/technomagi/common/blocks/BlockBaseContainer.java
+++ src/main/java/com/ollieread/technomagi/common/blocks/BlockBaseContainer.java
@@ -1,6 +1,16 @@
 package com.ollieread.technomagi.common.blocks;
 
-public class BlockBaseContainer
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+
+public abstract class BlockBaseContainer extends BlockContainer
 {
+
+    protected BlockBaseContainer(String name, Material material)
+    {
+        super(material);
+
+        this.setBlockName(name);
+    }
 
 }
