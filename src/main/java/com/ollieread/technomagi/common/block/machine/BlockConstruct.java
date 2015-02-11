1cbda48a5cbbd1acf1babc428246c71abd14894a
Ollie Read <labs@ollieread.com>
2015-02-11 11:23:32 +0000

    Added base construct for future machines

---
+++ src/main/java/com/ollieread/technomagi/common/block/machine/BlockConstruct.java
@@ -0,0 +1,24 @@
+package com.ollieread.technomagi.common.block.machine;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+import com.ollieread.technomagi.common.block.BlockBaseContainer;
+import com.ollieread.technomagi.common.block.machine.tile.TileConstruct;
+
+public class BlockConstruct extends BlockBaseContainer
+{
+
+    public BlockConstruct(String name)
+    {
+        super(name, Material.iron);
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World world, int metadata)
+    {
+        return new TileConstruct();
+    }
+
+}
---
+++ src/main/java/com/ollieread/technomagi/common/block/machine/tile/TileConstruct.java
@@ -0,0 +1,8 @@
+package com.ollieread.technomagi.common.block.machine.tile;
+
+import com.ollieread.technomagi.common.block.tile.TileBase;
+
+public class TileConstruct extends TileBase
+{
+
+}
