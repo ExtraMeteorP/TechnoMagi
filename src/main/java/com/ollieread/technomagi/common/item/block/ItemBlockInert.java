a5ee11c8dc6246f9c0c737d3ba805638bc7a8ba3
Ollie Read <labs@ollieread.com>
2015-03-30 16:36:39 +0100

    Gradle work and some fixes

--- src/main/java/com/ollieread/technomagi/common/item/block/ItemBlockInert.java
+++ src/main/java/com/ollieread/technomagi/common/item/block/ItemBlockInert.java
@@ -3,7 +3,10 @@ package com.ollieread.technomagi.common.item.block;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
+
+import com.ollieread.technomagi.Technomagi;
 
 public class ItemBlockInert extends ItemBlockBase
 {
@@ -14,21 +17,15 @@ public class ItemBlockInert extends ItemBlockBase
     }
 
     @Override
-    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
     {
-        if (world.isAirBlock(x, y, z)) {
-            int i1 = this.getMetadata(stack.getItemDamage());
-            int j1 = this.field_150939_a.onBlockPlaced(world, x, y, z, side, hitX, hitY, hitZ, i1);
+        MovingObjectPosition moving = getMovingObjectPositionFromPlayer(world, player, true);
 
-            if (placeBlockAt(stack, player, world, x, y, z, side, hitX, hitY, hitZ, j1)) {
-                world.playSoundEffect(x + 0.5F, y + 0.5F, z + 0.5F, this.field_150939_a.stepSound.func_150496_b(), (this.field_150939_a.stepSound.getVolume() + 1.0F) / 2.0F, this.field_150939_a.stepSound.getPitch() * 0.8F);
-                --stack.stackSize;
-            }
-
-            return true;
-        } else {
-            return false;
+        if (moving != null) {
+            Technomagi.debug("Pos: " + moving.blockX + ":" + moving.blockY + ":" + moving.blockZ);
         }
+
+        return stack;
     }
 
 }
