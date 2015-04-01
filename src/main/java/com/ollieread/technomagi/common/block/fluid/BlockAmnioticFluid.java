97b08ca621e0639df76d171254457c6e4bc06f74
Ollie Read <labs@ollieread.com>
2015-04-02 00:55:36 +0100

    1 in 50 chance of amniotic fluid healing/harming the entity inside it, as well as adding water breathing

--- src/main/java/com/ollieread/technomagi/common/block/fluid/BlockAmnioticFluid.java
+++ src/main/java/com/ollieread/technomagi/common/block/fluid/BlockAmnioticFluid.java
@@ -1,12 +1,20 @@
 package com.ollieread.technomagi.common.block.fluid;
 
+import java.util.Random;
+
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidFinite;
 
 import com.ollieread.technomagi.Technomagi;
 import com.ollieread.technomagi.common.init.Fluids;
 import com.ollieread.technomagi.common.tabs.TechnomagiTabs;
+import com.ollieread.technomagi.util.BlockHelper;
 
 public class BlockAmnioticFluid extends BlockFluidFinite
 {
@@ -20,6 +28,7 @@ public class BlockAmnioticFluid extends BlockFluidFinite
         this.name = name;
         this.setBlockTextureName(getTexturePath(name));
         this.setCreativeTab(TechnomagiTabs.blocks);
+        this.setTickRandomly(true);
     }
 
     public String getTexturePath(String name)
@@ -39,6 +48,33 @@ public class BlockAmnioticFluid extends BlockFluidFinite
         super.registerBlockIcons(register);
 
         Fluids.amniotic.setIcons(this.blockIcon);
+    }
+
+    @Override
+    public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity)
+    {
+        if (entity instanceof EntityLivingBase) {
+            EntityLivingBase entityLiving = (EntityLivingBase) entity;
+            if (world.rand.nextInt(50) == 0 && !entityLiving.isPotionActive(Potion.heal)) {
+                entityLiving.addPotionEffect(new PotionEffect(Potion.heal.id, 1, 1));
+            }
+
+            if (!entityLiving.isPotionActive(Potion.waterBreathing)) {
+                entityLiving.addPotionEffect(new PotionEffect(Potion.waterBreathing.id, 20, 1));
+            }
+        }
+    }
+
+    @Override
+    public void updateTick(World world, int x, int y, int z, Random rand)
+    {
+        int x2 = x + rand.nextInt(8);
+        int y2 = y + rand.nextInt(2);
+        int z2 = z + rand.nextInt(8);
+
+        BlockHelper.applyBonemeal(world, x2, y2, z2);
+
+        super.updateTick(world, x, y, z, rand);
     }
 
 }
