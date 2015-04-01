1af79b626d4a113224c29268776b20fa05a0d0e3
Ollie Read <labs@ollieread.com>
2015-04-02 00:56:09 +0100

    Entities submersed in enriched fluid will have the nanite regeneration effect applied to them

--- src/main/java/com/ollieread/technomagi/common/block/fluid/BlockEnrichedFluid.java
+++ src/main/java/com/ollieread/technomagi/common/block/fluid/BlockEnrichedFluid.java
@@ -2,10 +2,16 @@ package com.ollieread.technomagi.common.block.fluid;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidFinite;
 
 import com.ollieread.technomagi.Technomagi;
 import com.ollieread.technomagi.common.init.Fluids;
+import com.ollieread.technomagi.common.misc.PotionTechnomagi;
 import com.ollieread.technomagi.common.tabs.TechnomagiTabs;
 
 public class BlockEnrichedFluid extends BlockFluidFinite
@@ -39,6 +45,22 @@ public class BlockEnrichedFluid extends BlockFluidFinite
         super.registerBlockIcons(register);
 
         Fluids.enriched.setIcons(this.blockIcon);
+    }
+
+    @Override
+    public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity)
+    {
+        if (entity instanceof EntityLivingBase) {
+            EntityLivingBase entityLiving = (EntityLivingBase) entity;
+
+            if (!entityLiving.isPotionActive(PotionTechnomagi.naniteRegeneration)) {
+                entityLiving.addPotionEffect(new PotionEffect(PotionTechnomagi.naniteRegeneration.id, 200, 2));
+            }
+
+            if (!entityLiving.isPotionActive(Potion.waterBreathing)) {
+                entityLiving.addPotionEffect(new PotionEffect(Potion.waterBreathing.id, 20, 1));
+            }
+        }
     }
 
 }
