c26a0fc1a9357a6aa3c2f413d8a4947ce5e63a94
Ollie Read <labs@ollieread.com>
2015-04-02 00:56:29 +0100

    Added a helper method to apply bonemeal to coords (currently used for amniotic fluid only)

--- src/main/java/com/ollieread/technomagi/util/BlockHelper.java
+++ src/main/java/com/ollieread/technomagi/util/BlockHelper.java
@@ -1,7 +1,9 @@
 package com.ollieread.technomagi.util;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.IGrowable;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class BlockHelper
 {
@@ -29,6 +31,36 @@ public class BlockHelper
     public static BlockRepresentation getBlockRepresentation(Block block, int metadata)
     {
         return new BlockRepresentation(block, metadata);
+    }
+
+    /**
+     * Applies a bonemeal style effect to a block.
+     *
+     * @param world
+     * @param x
+     * @param y
+     * @param z
+     * @return
+     */
+    public static boolean applyBonemeal(World world, int x, int y, int z)
+    {
+        Block block = world.getBlock(x, y, z);
+
+        if (block != null && block instanceof IGrowable) {
+            IGrowable igrowable = (IGrowable) block;
+
+            if (igrowable.func_149851_a(world, x, y, z, world.isRemote)) {
+                if (!world.isRemote) {
+                    if (igrowable.func_149852_a(world, world.rand, x, y, z)) {
+                        igrowable.func_149853_b(world, world.rand, x, y, z);
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
