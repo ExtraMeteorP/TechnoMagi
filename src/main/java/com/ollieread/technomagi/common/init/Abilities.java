8d98a86936b277d92ab8afce283014e4c18ad3dc
Ollie Read <labs@ollieread.com>
2015-04-11 19:12:52 +0100

    Removed the scan ability

--- src/main/java/com/ollieread/technomagi/common/init/Abilities.java
+++ src/main/java/com/ollieread/technomagi/common/init/Abilities.java
@@ -4,23 +4,23 @@ import com.ollieread.technomagi.api.TechnomagiApi;
 import com.ollieread.technomagi.api.ability.IAbilityCast;
 import com.ollieread.technomagi.common.ability.cast.AbilityCastNanitesForFood;
 import com.ollieread.technomagi.common.ability.cast.AbilityCastRegenerate;
-import com.ollieread.technomagi.common.ability.cast.AbilityCastScan;
 import com.ollieread.technomagi.util.ResourceHelper;
 
 public class Abilities
 {
 
-    public static IAbilityCast scan;
+    // public static IAbilityCast scan;
     public static IAbilityCast regenerate;
     public static IAbilityCast naniteFood;
 
     public static void init()
     {
-        scan = new AbilityCastScan("scan", ResourceHelper.texture("abilities/scan.png"));
+        // scan = new AbilityCastScan("scan",
+        // ResourceHelper.texture("abilities/scan.png"));
         regenerate = new AbilityCastRegenerate("regenerate", ResourceHelper.texture("abilities/regenerate.png"));
         naniteFood = new AbilityCastNanitesForFood("nanites_for_food", ResourceHelper.texture("abilities/food.png"));
 
-        TechnomagiApi.ability().addCastableAbility(scan);
+        // TechnomagiApi.ability().addCastableAbility(scan);
         TechnomagiApi.ability().addCastableAbility(regenerate);
         TechnomagiApi.ability().addCastableAbility(naniteFood);
     }
