ff6776c50bcb47d61dfc3ecc9ccb13ef305ebff6
Ollie Read <labs@ollieread.com>
2015-04-26 12:22:53 +0100

    Added textures for light structure blocks

--- src/main/java/com/ollieread/technomagi/common/block/structure/BlockStructure.java
+++ src/main/java/com/ollieread/technomagi/common/block/structure/BlockStructure.java
@@ -2,16 +2,26 @@ package com.ollieread.technomagi.common.block.structure;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import com.ollieread.technomagi.common.block.BlockContainerSubtypes;
 import com.ollieread.technomagi.common.block.structure.tile.TileStructure;
 import com.ollieread.technomagi.common.block.structure.tile.TileStructureBridge;
 import com.ollieread.technomagi.common.block.structure.tile.TileStructurePlatform;
+import com.ollieread.technomagi.util.ResourceHelper;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockStructure extends BlockContainerSubtypes
 {
+
+    @SideOnly(Side.CLIENT)
+    public IIcon[] blockIcons = new IIcon[3];
 
     public BlockStructure(String name)
     {
@@ -29,6 +39,60 @@ public class BlockStructure extends BlockContainerSubtypes
             default:
                 return null;
         }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister register)
+    {
+        blockIcons[0] = register.registerIcon(ResourceHelper.icon("machine/electric/generic_side"));
+        blockIcons[1] = register.registerIcon(ResourceHelper.icon("machine/electric/light_generator"));
+        blockIcons[2] = register.registerIcon(ResourceHelper.icon("machine/electric/light_generator_off"));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int metadata)
+    {
+        if (metadata == 1) {
+            if (side == 4) {
+                return this.blockIcons[1];
+            }
+        } else {
+            if (side != 0 && side != 1) {
+                return this.blockIcons[1];
+            }
+        }
+
+        return this.blockIcons[0];
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(IBlockAccess world, int x, int y, int z, int side)
+    {
+        int metadata = world.getBlockMetadata(x, y, z);
+        TileStructure tile = (TileStructure) world.getTileEntity(x, y, z);
+
+        if (metadata == 1) {
+            if (side != 0 && side != 1) {
+                if (tile != null && tile.getDirection() != null) {
+                    if (tile.getDirection().ordinal() == side) {
+                        if (tile.isEnabled()) {
+                            return this.blockIcons[1];
+                        } else {
+                            return this.blockIcons[2];
+                        }
+                    }
+                }
+            }
+        } else {
+            if (side != 0 && side != 1) {
+                return tile.isEnabled() ? this.blockIcons[1] : this.blockIcons[2];
+            }
+        }
+
+        return this.blockIcons[0];
     }
 
     @Override
