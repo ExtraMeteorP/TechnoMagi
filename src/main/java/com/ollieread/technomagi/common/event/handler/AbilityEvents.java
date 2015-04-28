21dca72ce9ac83ad75a56ce0225a43d3b8fd9ccd
Ollie Read <labs@ollieread.com>
2015-04-28 11:10:03 +0100

    Interaction events are cancelled if the pre-requisites aren't met

--- src/main/java/com/ollieread/technomagi/common/event/handler/AbilityEvents.java
+++ src/main/java/com/ollieread/technomagi/common/event/handler/AbilityEvents.java
@@ -57,6 +57,8 @@ public class AbilityEvents
 
                     technomage.abilities().startCasting(heldStack, payload);
                 }
+            } else {
+                event.setCanceled(true);
             }
         }
     }
