d0c01030c24cf1d2b2de8adef3e0fe450b0a2b07
Ollie Read <labs@ollieread.com>
2015-05-02 18:40:27 +0100

    Added a tile specific extension of IResearcher

---
+++ src/main/java/com/ollieread/technomagi/api/tile/ITileResearcher.java
@@ -0,0 +1,10 @@
+package com.ollieread.technomagi.api.tile;
+
+import com.ollieread.technomagi.api.knowledge.research.IResearcher;
+
+public interface ITileResearcher extends IResearcher
+{
+
+    public void resetResearch();
+
+}
